0d2015240df2fb537ada5277bbbff4bb23dba045
Chico Charlesworth <chico.charlesworth@googlemail.com>
2011-09-10 07:22:21 +0100

    initial groupconnect import

---
+++ groupconnect/app/controllers/Application.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+	render();
+    }
+    
+    public static void view() {
+	render();
+    }
+
+    public static void about() {
+	render();
+    }
+    
+    public static void schedule() {
+	render();
+    }
+    
+    public static void confirm() {
+	render();
+    }
+
+}
