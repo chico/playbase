e15fc3bcda0845a582f945283f471a1c2b6612d9
Chico Charlesworth <chico.charlesworth@googlemail.com>
2011-09-10 09:28:49 +0100

    Added plan page

--- groupconnect/app/controllers/Application.java
+++ groupconnect/app/controllers/Application.java
@@ -25,6 +25,10 @@ public class Application extends Controller {
 	render();
     }
     
+    public static void plan() {
+	render();
+    }
+    
     public static void confirm() {
 	render();
     }
